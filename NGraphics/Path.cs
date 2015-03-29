036ff3229458241012f12cc5501212d3d2acbba3
Paul Patarinski <ppatarinski@paylocity.com>
2015-03-29 12:58:29 -0500

    Adding MoveTo support for 2 points

--- NGraphics/Path.cs
+++ NGraphics/Path.cs
@@ -13,16 +13,24 @@ namespace NGraphics
 	{
 		public Point Point;
 	  public bool IsAbsolute;
-
+		public Point Start;
+		public Point End;
 	  public MoveTo (Point point, bool isAbsolute)
 	  {
 	    Point = point;
 	    IsAbsolute = isAbsolute;
 	  }
 
-	  public MoveTo (double x, double y, bool isAbsolute)
+		public MoveTo (double x, double y, bool isAbsolute)
 			: this (new Point (x, y), isAbsolute)
 		{
+		}
+
+		public MoveTo (Point start, Point end, bool isAbsolute)
+		{
+			Start = start;
+			End = end;
+			IsAbsolute = isAbsolute;
 		}
 
 		public override Point GetContinueCurveControlPoint ()
@@ -172,6 +180,11 @@ namespace NGraphics
 		public void MoveTo (double x, double y, bool isAbsolute)
 		{
 			Add (new MoveTo (x, y, isAbsolute));
+		}
+
+		public void MoveTo (Point start, Point end, bool isAbsolute)
+		{
+			Add (new MoveTo (start,end, isAbsolute));
 		}
 
 		public void LineTo ( Point point)
