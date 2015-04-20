4810560654f03c14076f2fff079bd637038bd6d8
ppatarinski <ppatarinski@paylocity.com>
2015-04-20 00:27:39 -0500

    Refactoring the .NET implementation

--- Tests/NGraphics.Net.Test/Program.cs
+++ Tests/NGraphics.Net.Test/Program.cs
@@ -10,44 +10,55 @@ namespace NGraphics.Net.Test
 	{
 		public static void Main (string[] args)
 		{
-			var sdir = System.IO.Path.GetDirectoryName (Environment.GetCommandLineArgs () [0]);
-			while (Directory.GetFiles (sdir, "NGraphics.sln").Length == 0)
-				sdir = System.IO.Path.GetDirectoryName (sdir);
-			PlatformTest.ResultsDirectory = System.IO.Path.Combine (sdir, "TestResults");
-			PlatformTest.Platform = Platforms.Current;
-			Environment.CurrentDirectory = PlatformTest.ResultsDirectory;
-
-			var tat = typeof(NUnit.Framework.TestAttribute);
-      var tfat = typeof(NUnit.Framework.TestFixtureAttribute);
-      var testSetupAttr = typeof(NUnit.Framework.SetUpAttribute);
-
-			var types = typeof (DrawingTest).Assembly.GetTypes ();
-			var testFixtures = types.Where (t => t.GetCustomAttributes (tfat, false).Length > 0);
-
-			foreach (var testFixture in testFixtures) {
-				var testFixtureInstance = Activator.CreateInstance (testFixture);
-        var tests = testFixture.GetMethods().Where(m => m.GetCustomAttributes(tat, true).Length > 0);
-        var testSetup = testFixture.GetMethods().Where(m => m.GetCustomAttributes(testSetupAttr, true).Length > 0).ToList();
-
-				foreach (var test in tests) {
-
-          if (testSetup.Any())
-          {
-            testSetup.First().Invoke(testFixtureInstance, null);
-          }
-
-          //if (test.Name.Equals("ErulisseuiinSpaceshipPack"))
-          //{
-            Console.WriteLine("Running {0}...", test);
-
-            test.Invoke(testFixtureInstance, null);
-          //}
-				
-				}
-			}
-
-			Console.WriteLine ("Done...");
-      Thread.Sleep(TimeSpan.FromSeconds(1));
+		  PlatformSetup();
+		  RunTests();
 		}
+
+    private static void PlatformSetup()
+    {
+      var sdir = System.IO.Path.GetDirectoryName(Environment.GetCommandLineArgs()[0]);
+      while (Directory.GetFiles(sdir, "NGraphics.sln").Length == 0)
+        sdir = System.IO.Path.GetDirectoryName(sdir);
+      PlatformTest.ResultsDirectory = System.IO.Path.Combine(sdir, "TestResults");
+      PlatformTest.Platform = Platforms.Current;
+      Environment.CurrentDirectory = PlatformTest.ResultsDirectory;
+    }
+
+	  private static void RunTests()
+	  {
+	    var tat = typeof (NUnit.Framework.TestAttribute);
+	    var tfat = typeof (NUnit.Framework.TestFixtureAttribute);
+	    var testSetupAttr = typeof (NUnit.Framework.SetUpAttribute);
+
+	    var types = typeof (DrawingTest).Assembly.GetTypes();
+	    var testFixtures = types.Where(t => t.GetCustomAttributes(tfat, false).Length > 0);
+
+	    foreach (var testFixture in testFixtures)
+	    {
+	      var testFixtureInstance = Activator.CreateInstance(testFixture);
+	      var tests = testFixture.GetMethods().Where(m => m.GetCustomAttributes(tat, true).Length > 0);
+	      var testSetup = testFixture.GetMethods().Where(m => m.GetCustomAttributes(testSetupAttr, true).Length > 0).ToList();
+
+	      foreach (var test in tests)
+	      {
+	        if (testSetup.Any())
+	        {
+	          testSetup.First().Invoke(testFixtureInstance, null);
+	        }
+
+	        //if (test.Name.Equals("ErulisseuiinSpaceshipPack"))
+	        //{
+	        Console.WriteLine("Running {0}...", test);
+
+	        test.Invoke(testFixtureInstance, null);
+	        //}
+	      }
+	    }
+
+	    Console.WriteLine("Done...");
+	    Thread.Sleep(TimeSpan.FromSeconds(1));
+	  }
+
+	
 	}
 }
