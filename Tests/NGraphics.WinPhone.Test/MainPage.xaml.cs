fa2dfe075d1e23307b5c043ad84f6312ed0d4e55
ppatarinski <ppatarinski@paylocity.com>
2015-05-10 12:06:04 -0500

    Using a Listview for Win Phone

--- Tests/NGraphics.WinPhone.Test/MainPage.xaml.cs
+++ Tests/NGraphics.WinPhone.Test/MainPage.xaml.cs
@@ -1,48 +1,113 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
+using System.Reflection;
+using System.Threading.Tasks;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Imaging;
 using Windows.UI.Xaml.Navigation;
-
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=391641
+using NGraphics.Test;
+using NGraphics.WinPhone.Test.Models;
+using NUnit.Framework;
 
 namespace NGraphics.WinPhone.Test
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+  public sealed partial class MainPage : Page
+  {
+    public MainPage()
     {
-        public MainPage()
-        {
-            this.InitializeComponent();
+      InitializeComponent();
 
-            this.NavigationCacheMode = NavigationCacheMode.Required;
-        }
-
-        /// <summary>
-        /// Invoked when this page is about to be displayed in a Frame.
-        /// </summary>
-        /// <param name="e">Event data that describes how this page was reached.
-        /// This parameter is typically used to configure the page.</param>
-        protected override void OnNavigatedTo(NavigationEventArgs e)
-        {
-            // TODO: Prepare page for display here.
-
-            // TODO: If your application contains multiple pages, ensure that you are
-            // handling the hardware Back button by registering for the
-            // Windows.Phone.UI.Input.HardwareButtons.BackPressed event.
-            // If you are using the NavigationHelper provided by some templates,
-            // this event is handled for you.
-        }
+      ImageGridView.ItemsSource = SampleImages;
+      NavigationCacheMode = NavigationCacheMode.Required;
     }
+
+    private ObservableCollection<SampleImage> _sampleImages;
+
+    public ObservableCollection<SampleImage> SampleImages
+    {
+      get { return _sampleImages ?? (_sampleImages = new ObservableCollection<SampleImage>()); }
+    }
+
+    /// <summary>
+    ///   Invoked when this page is about to be displayed in a Frame.
+    /// </summary>
+    /// <param name="e">
+    ///   Event data that describes how this page was reached.
+    ///   This parameter is typically used to configure the page.
+    /// </param>
+    protected override async void OnNavigatedTo(NavigationEventArgs e)
+    {
+      PlatformSetup();
+      await RunTests();
+    }
+
+    private void PlatformSetup()
+    {
+      PlatformTest.ResultsDirectory = "TestResults";
+      PlatformTest.Platform = Platforms.Current;
+
+      PlatformTest.OpenStream = path => new FileMemoryStream {Path = path};
+      PlatformTest.CloseStream = async (stream, name) =>
+      {
+        //await UploadToLocalhost(name, stream, client);
+
+        using (var memStream =
+          stream.AsRandomAccessStream())
+        {
+          var bitmapImage = new BitmapImage();
+          memStream.Seek(0);
+          bitmapImage.SetSource(memStream);
+
+          SampleImages.Add(new SampleImage
+          {
+            Name = name,
+            ImageSource = bitmapImage
+          });
+        }
+      };
+    }
+
+    private static async Task RunTests()
+    {
+      var tat = typeof(TestAttribute);
+      var tfat = typeof(TestFixtureAttribute);
+      var testSetupAttr = typeof(SetUpAttribute);
+
+      var types = typeof(DrawingTest).GetTypeInfo().Assembly.ExportedTypes;
+      var testFixtures = types.Where(t => t.GetTypeInfo().GetCustomAttributes(tfat, false).Any());
+
+      foreach (var testFixture in testFixtures)
+      {
+        var testFixtureInstance = Activator.CreateInstance(testFixture);
+        var tests = testFixture.GetRuntimeMethods().Where(m => m.GetCustomAttributes(tat, true).Any());
+        var testSetup =
+          testFixture.GetRuntimeMethods().Where(m => m.GetCustomAttributes(testSetupAttr, true).Any()).ToList();
+
+        foreach (var test in tests)
+        {
+          if (testSetup.Any())
+          {
+            testSetup.First().Invoke(testFixtureInstance, null);
+          }
+
+          //if (test.Name.Contains("PathData"))
+          //{
+          Debug.WriteLine("Running {0}...", test);
+
+          test.Invoke(testFixtureInstance, null);
+          //}
+        }
+      }
+
+      Debug.WriteLine("Done...");
+    }
+
+    private class FileMemoryStream : MemoryStream
+    {
+      public string Path;
+    }
+  }
 }
---
+++ Tests/NGraphics.WinPhone.Test/Models/SampleImage.cs
@@ -0,0 +1,10 @@
+﻿using Windows.UI.Xaml.Media.Imaging;
+
+namespace NGraphics.WinPhone.Test.Models
+{
+  public class SampleImage
+  {
+    public string Name { get; set; }
+    public BitmapImage ImageSource { get; set; }
+  }
+}
